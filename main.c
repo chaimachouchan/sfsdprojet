ac1ce2d1a450856bf5dbc9f20ef70604ee15e287
Chouchan chaima <chaimachouchane04@gmail.com>
2024-01-21 21:56:29 +0100

    fct ientete et affentete

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -9,13 +9,13 @@
       char *prenom;
       bool supp;
   }typeEng;
- // decalaration de bloc
+
+  // decalaration de bloc
   typedef struct TypeBloc{
       typeEng tab[10];
       int nb;// nb d'enrg
   }TypeBloc;
-
- //declaration buffer
+  //declaration buffer
    typedef struct TypeBloc buffer;
 
   // declaration entete n3amroha dans main entete au debut de fichier
@@ -31,3 +31,38 @@
        FILE *f;
        entete entete;
    }TOV;
+
+
+
+    // Fonction pour obtenir la i-ème caractéristique de l'en-tête ientete(f,i)
+
+int ientete(TOV *f, int i) { // tov *f int i
+
+    // Retourne la i-ème caractéristique en fonction de la valeur de i
+    switch (i) {
+        case 1:
+            return (f->entete).indice_der_bloc;
+        case 2:
+            return (f->entete).totalblocs;
+         case 3:
+             return (f->entete).totaleng;
+        // Ajoutez d'autres cas pour d'autres caractéristiques
+        default:
+            fprintf(stderr, "Caractéristique non prise en charge\n");
+            return -1;  // Valeur par défaut pour caractéristique non prise en charge
+    }}
+
+
+           // mettre a jour l'entete affentete
+
+        int AffEntete(TOV *f, int i, int val) {
+             // si indice i = 1er cara alors on modifier la premiere cara
+            if (i==1)
+               return(f->entete).indice_der_bloc=val;
+            //si indice i = 2eme cara alors on modifier la 2eme cara
+            else if (i==2)
+               return(f->entete).totalblocs=val;
+            else if (i==3)
+                return (f->entete).totaleng=val;
+
+         }
