5515394d49e8216f8a9a296d2ff124e6debf7ee7
Benmesbah Amira <amira.benmesbah18@gmail.com>
2024-01-21 18:53:29 +0100

    reverse code

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+ //declaration  chauque enrg dans tab
+  typedef struct typeEng{
+      int cle;
+      char *nom;
+      char *prenom;
+      bool supp;
+  }typeEng;
+ // decalaration de bloc
+  typedef struct TypeBloc{
+      typeEng tab[10];
+      int nb;// nb d'enrg
+  }TypeBloc;
+
  //declaration buffer
    typedef struct TypeBloc buffer;
 
