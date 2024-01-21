c454ec762a197e7d2eec2edbf46a3a95a0167fd2
Chouchan chaima <chaimachouchane04@gmail.com>
2024-01-22 00:25:31 +0100

    fct supression et corriger declaration de buffer

--- main.c
+++ main.c
@@ -22,9 +22,7 @@
 
  //declaration buffer
 
-typedef struct Buffer {
-    TypeBloc bloc;
-} Buffer;
+typedef struct TypeBloc buffer;
 
   // declaration entete n3amroha dans main entete au debut de fichier
   typedef struct entete{
@@ -73,24 +71,24 @@ int ientete(TOV *f, int i) { // tov *f int i
 
          }
          // Lecture du bloc i dans le tampon (buf)
-void lireDir(TOV *fichier, int i, Buffer *buf) {
+void lireDir(TOV *fichier, int i, buffer *buf) {
     // Vérifie si i est dans les limites du fichier
     if (i <= ientete(fichier, 1)) {
         // Positionne le curseur au début du bloc i
         fseek(fichier->f, sizeof(entete) + ((i - 1) * sizeof(TypeBloc)), SEEK_SET);
         // Lit le bloc dans le tampon (buf)
-        fread(&(buf->bloc), sizeof(TypeBloc), 1, fichier->f);
+        fread(&buf, sizeof(TypeBloc), 1, fichier->f);
     }
 }
 
 // Écriture du tampon (buf) dans le bloc i
-void EcrireDir(TOV *fichier, int i, Buffer buf) {
+void EcrireDir(TOV *fichier, int i, buffer buf) {
     // Vérifie si i est dans les limites du fichier
     if (i <= ientete(fichier, 1)) {
         // Positionne le curseur au début du bloc i
         fseek(fichier->f, sizeof(entete) + (sizeof(TypeBloc) * (i - 1)), SEEK_SET);
         // Écrit le tampon dans le fichier
-        fwrite(&(buf.bloc), sizeof(TypeBloc), 1, fichier->f);
+        fwrite(&buf, sizeof(TypeBloc), 1, fichier->f);
     }
 }
  //fct creation un fichier
@@ -132,3 +130,33 @@ void fermer(TOV *fichier)
 }
 
 
+       //supression d un enregistrement de cle c
+  void supression(int c , TOV *fichier){
+     int i,j ;// i:indice de tableau de bloc j: indice d'eng dans un tab d'eng dans un bloc
+     int k ; // pour faire decalage de tab d'eng
+     char type[20] ;// type de supression
+     bool trouv;
+     TypeBloc buf ;
+     const char lo[20]= "logique";
+     const char ph[20]= "physique" ;
+     recherche_dicho(fichier,c,&trouv,&i,&j); //cherche a cle c
+      if(trouv == true){
+        printf("vous voullez la supression physique ou logique? ");
+        scanf("%s", type);//choisi si physique ou logique
+        //strcmp pour compare 2 chaine de caractere type et lo ou ph si return 0 alors sont identique
+         if(strcmp(type,lo) == 0){
+
+            buf.tab[j].supp = true;
+            EcrireDir(fichier,i,buf);
+         }else if(strcmp(type,ph) == 0){
+
+                for (k=j ; k<buf.nb ; k++){
+
+                    buf.tab[k]=buf.tab[k+1]; //decalage apartir d enrg qui veut suprimer
+                }
+                buf.nb-- ; //pour suprime derniere case
+                EcrireDir(fichier,i,buf);
+              }
+      }
+      fclose(fichier);
+   }
